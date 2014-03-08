4b723c80b0b39792ca58e28b845f8c6029ab41f4
Andrew Walker <andyw@lucy.local>
2014-03-08 21:25:52 +0000

    prb best do that like this, on reflection

--- civicrm.php
+++ civicrm.php
@@ -2,34 +2,40 @@
 
 class CiviCRM_Command extends WP_CLI_Command {
 
+    /**
+     * WP-CLI integration with CiviCRM.
+     */
     public function __invoke($args, $assoc_args) {
-        
+       
+        # existential check
+        if (!function_exists('civicrm_initialize')) 
+            return WP_CLI::error("Unable to find instance of CiviCRM.");
+
         $command_router = array(
-        	'enable-debug' => 'enableDebug'
+            'enable-debug' => 'enableDebug'
         );
 
-        # using global $argv to support drush style command aliases
-        # eg: 'wp cvapi' as an alias for 'wp civicrm api'
-        global $argv;
-        foreach ($argv as $arg) {
-        	if (isset($command_router[$arg])) {
-        		if (method_exists($this, $command_router[$arg])) {
-        			return $this->{$command_router[$arg]}($args, $assoc_args);
-        		} else {
-        			WP_CLI::error("Unimplemented command - '$arg'");
-        		}
-        	}
-        }        
+        $command = array_shift($args);
+
+        if (isset($command_router[$command])) {
+            if (method_exists($this, $command_router[$command])) {
+                civicrm_initialize(true);
+                return $this->{$command_router[$command]}($args, $assoc_args);
+            } else {
+                WP_CLI::error("Unimplemented command - '$arg'");
+            }
+        }
+               
     }
 
     private function enableDebug($args, $assoc_args) {
-    	/*
-    	$params['debug']     = 1;
+        /*
+        $params['debug']     = 1;
         $params['backtrace'] = 1;
 
         require_once 'CRM/Admin/Form/Setting.php';
         CRM_Admin_Form_Setting::commonProcess($params);
-		*/
+        */
         WP_CLI::success('Debug setting enabled.');
     }
 
