2824c82a08fda8a6da14d44cef0f11d4d67cc800
Andrew Walker <andyw@lucy.local>
2014-03-08 21:08:49 +0000

    Ugly, but basically working

--- civicrm.php
+++ civicrm.php
@@ -1,22 +1,38 @@
 <?php
 
-class CiviCRM_Enable_Debug extends WP_CLI_Command {
+class CiviCRM_Command extends WP_CLI_Command {
 
     public function __invoke($args, $assoc_args) {
         
-        $params['debug']     = 1;
+        $command_router = array(
+        	'enable-debug' => 'enableDebug'
+        );
+
+        # using global $argv to support drush style command aliases
+        # eg: 'wp cvapi' as an alias for 'wp civicrm api'
+        global $argv;
+        foreach ($argv as $arg) {
+        	if (isset($command_router[$arg])) {
+        		if (method_exists($this, $command_router[$arg])) {
+        			return $this->{$command_router[$arg]}($args, $assoc_args);
+        		} else {
+        			WP_CLI::error("Unimplemented command - '$arg'");
+        		}
+        	}
+        }        
+    }
+
+    private function enableDebug($args, $assoc_args) {
+    	/*
+    	$params['debug']     = 1;
         $params['backtrace'] = 1;
 
         require_once 'CRM/Admin/Form/Setting.php';
         CRM_Admin_Form_Setting::commonProcess($params);
-
+		*/
         WP_CLI::success('Debug setting enabled.');
-        
     }
 
 }
 
-foreach (array(
-    'civicrm-enable-debug' => 'CiviCRM_Enable_Debug',
-) as $command => $class)
-    WP_CLI::add_command($command, $class);
+WP_CLI::add_command('civicrm', 'CiviCRM_Command');
