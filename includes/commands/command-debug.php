ea50650be278ca82bfc34073d71a5982241b0df0
Christian Wach <needle@haystack.co.uk>
2023-01-13 18:41:04 +0000

    Add debug command

---
+++ includes/commands/command-debug.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Enable or disable debugging in CiviCRM.
+ *
+ * ## EXAMPLES
+ *
+ *   $ wp civicrm debug enable
+ *   Success: Debug setting enabled.
+ *
+ *   $ wp civicrm debug disable
+ *   Success: Debug setting disabled.
+ *
+ * @since 1.0.0
+ */
+class CLI_Tools_CiviCRM_Command_Debug extends CLI_Tools_CiviCRM_Command {
+
+  /**
+   * Dependency check.
+   *
+   * @since 1.0.0
+   */
+  public static function check_dependencies() {
+    // Check for existence of CiviCRM.
+    if (!function_exists('civicrm_initialize')) {
+      WP_CLI::error('Unable to find CiviCRM install.');
+    }
+  }
+
+  /**
+   * Enable debugging in CiviCRM.
+   *
+   * ## EXAMPLES
+   *
+   *   $ wp civicrm debug enable
+   *   Success: Debug setting enabled.
+   *
+   * @since 1.0.0
+   *
+   * @param array $args The WP-CLI positional arguments.
+   * @param array $assoc_args The WP-CLI associative arguments.
+   */
+  public function enable($args, $assoc_args) {
+
+    civicrm_initialize();
+
+    Civi::settings()->add([
+      'debug_enabled' => 1,
+      'backtrace' => 1,
+    ]);
+
+    WP_CLI::success('Debug setting enabled.');
+
+  }
+
+  /**
+   * Disable debugging in CiviCRM.
+   *
+   * ## EXAMPLES
+   *
+   *   $ wp civicrm debug disable
+   *   Success: Debug setting disabled.
+   *
+   * @since 1.0.0
+   *
+   * @param array $args The WP-CLI positional arguments.
+   * @param array $assoc_args The WP-CLI associative arguments.
+   */
+  public function disable($args, $assoc_args) {
+
+    civicrm_initialize();
+
+    Civi::settings()->add([
+      'debug_enabled' => 0,
+      'backtrace' => 0,
+    ]);
+
+    WP_CLI::success('Debug setting disabled.');
+
+  }
+
+}
--- includes/wp-cli-civicrm.php
+++ includes/wp-cli-civicrm.php
@@ -21,6 +21,7 @@ WP_CLI::add_hook('before_wp_load', function() {
   require_once __DIR__ . '/commands/command-civicrm.php';
   require_once __DIR__ . '/commands/command-api-v3.php';
   require_once __DIR__ . '/commands/command-cache-clear.php';
+  require_once __DIR__ . '/commands/command-debug.php';
   require_once __DIR__ . '/commands/command-upgrade-db.php';
   require_once __DIR__ . '/commands/command-version.php';
 
@@ -39,6 +40,10 @@ WP_CLI::add_hook('before_wp_load', function() {
   // Add Cache Clear command.
   WP_CLI::add_command('civicrm cache-clear', 'CLI_Tools_CiviCRM_Command_Cache_Clear', ['before_invoke' => 'CLI_Tools_CiviCRM_Command_Cache_Clear::check_dependencies']);
   WP_CLI::add_command('cv cache-clear', 'CLI_Tools_CiviCRM_Command_Cache_Clear', ['before_invoke' => 'CLI_Tools_CiviCRM_Command_Cache_Clear::check_dependencies']);
+
+  // Add Debug command.
+  WP_CLI::add_command('civicrm debug', 'CLI_Tools_CiviCRM_Command_Debug', ['before_invoke' => 'CLI_Tools_CiviCRM_Command_Debug::check_dependencies']);
+  WP_CLI::add_command('cv debug', 'CLI_Tools_CiviCRM_Command_Debug', ['before_invoke' => 'CLI_Tools_CiviCRM_Command_Debug::check_dependencies']);
 
   // Add database upgrade command.
   WP_CLI::add_command('civicrm upgrade-db', 'CLI_Tools_CiviCRM_Command_Upgrade_DB', ['before_invoke' => 'CLI_Tools_CiviCRM_Command_Upgrade_DB::check_dependencies']);
