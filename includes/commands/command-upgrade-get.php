49fd27b35d2805b39054f4d4e248376e4b9bc839
Christian Wach <needle@haystack.co.uk>
2023-01-26 12:01:55 +0000

    Convert request to WP-CLI native method

--- includes/commands/command-upgrade-get.php
+++ includes/commands/command-upgrade-get.php
@@ -52,16 +52,14 @@ class CLI_Tools_CiviCRM_Command_Upgrade_Get extends CLI_Tools_CiviCRM_Command {
     $raw = \WP_CLI\Utils\get_flag_value($assoc_args, 'raw', FALSE);
 
     // Look up the data.
-    $url = $this->check_url . "?stability=" . urlencode($stability);
-    $result = file_get_contents($url);
+    $url = $this->check_url . '?stability=' . $stability;
+    $response = $this->json_get_response($url);
+    $lookup = json_decode($response, TRUE);
 
-    // Try and decode response.
-    $lookup = json_decode($result, TRUE);
+    // Sanity checks.
     if (JSON_ERROR_NONE !== json_last_error()) {
       WP_CLI::error(sprintf(WP_CLI::colorize('Failed to decode JSON: %y%s.%n'), json_last_error_msg()));
     }
-
-    // Sanity checks.
     if (empty($lookup)) {
       WP_CLI::error(sprintf(WP_CLI::colorize('Version not found at: %y%s%n'), $url));
     }
@@ -73,7 +71,7 @@ class CLI_Tools_CiviCRM_Command_Upgrade_Get extends CLI_Tools_CiviCRM_Command {
       WP_CLI::log($lookup['tar']['WordPress']);
     }
     else {
-      WP_CLI::log($result);
+      WP_CLI::log($response);
     }
 
   }
