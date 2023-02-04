e696ac205e6255526eb3ecdcf19576104e20a5d5
Christian Wach <needle@haystack.co.uk>
2023-02-04 19:27:31 +0000

    Add verbosity flags to legacy upgrade command

--- includes/commands/legacy/command-upgrade.php
+++ includes/commands/legacy/command-upgrade.php
@@ -25,6 +25,12 @@ class CLI_Tools_CiviCRM_Command_Upgrade extends CLI_Tools_CiviCRM_Command {
    * [--backup-dir=<backup-dir>]
    * : Path to your CiviCRM backup directory. Default is one level above ABSPATH.
    *
+   * [--v]
+   * : Run the upgrade queue with verbose output.
+   *
+   * [--vv]
+   * : Run the upgrade queue with extra verbose output.
+   *
    * ## EXAMPLES
    *
    *     # Update to the version of CiviCRM in the supplied archive.
@@ -43,10 +49,17 @@ class CLI_Tools_CiviCRM_Command_Upgrade extends CLI_Tools_CiviCRM_Command {
     $zipfile = (string) \WP_CLI\Utils\get_flag_value($assoc_args, 'zipfile', '');
     $tarfile = (string) \WP_CLI\Utils\get_flag_value($assoc_args, 'l10n-tarfile', '');
     $backup_dir = (string) \WP_CLI\Utils\get_flag_value($assoc_args, 'backup-dir', '');
+    $v = (bool) \WP_CLI\Utils\get_flag_value($assoc_args, 'v', FALSE);
+    $vv = (bool) \WP_CLI\Utils\get_flag_value($assoc_args, 'vv', FALSE);
 
     // Bail when .tar.gz archive is specified.
     if (!empty($tarfile)) {
       WP_CLI::error('CiviCRM .tar.gz archives are not supported.');
+    }
+
+    // Choose higher verbosity when both are specified.
+    if (!empty($v) && !empty($vv)) {
+      $v = FALSE;
     }
 
     // Use "wp civicrm core backup" to backup the CiviCRM installation.
@@ -60,7 +73,7 @@ class CLI_Tools_CiviCRM_Command_Upgrade extends CLI_Tools_CiviCRM_Command {
     WP_CLI::runcommand($command, $options);
 
     // Use "wp civicrm core update-db" to upgrade the CiviCRM database.
-    $command = 'civicrm core update-db --v';
+    $command = 'civicrm core update-db' . (empty($v) ? '' : ' --v') . (empty($vv) ? '' : ' --vv');
     $options = ['launch' => TRUE, 'return' => FALSE];
     WP_CLI::runcommand($command, $options);
 
