db2e83545c214469b12d2c98b61ed3fcf0700e8c
Christian Wach <needle@haystack.co.uk>
2024-02-06 12:56:03 +0000

    Add basic Extension download sub-command

--- includes/commands/command-ext.php
+++ includes/commands/command-ext.php
@@ -16,11 +16,86 @@
  *     $ wp civicrm ext list --refresh
  *     Success: CiviCRM Extensions refreshed.
  *
+ *     $ wp civicrm ext download org.example.foobar
+ *     Success: CiviCRM Extension downloaded.
+ *
  * @since 1.0.0
  *
  * @package Command_Line_Tools_for_CiviCRM
  */
 class CLI_Tools_CiviCRM_Command_Ext extends CLI_Tools_CiviCRM_Command {
+
+  /**
+   * Download a CiviCRM Extension.
+   *
+   * ## EXAMPLES
+   *
+   *     $ wp civicrm ext download org.example.foobar
+   *     Success: CiviCRM Extension downloaded.
+   *
+   *     $ wp civicrm ext download foobar --install
+   *     Success: CiviCRM Extension downloaded and installed.
+   *
+   * ## OPTIONS
+   *
+   * <key-or-name>
+   * : The extension full key ("org.example.foobar") or short name ("foobar").
+   *
+   * [--install]
+   * : Download and install the specified CiviCRM Extension.
+   *
+   * @alias dl
+   *
+   * @since 1.0.0
+   *
+   * @param array $args The WP-CLI positional arguments.
+   * @param array $assoc_args The WP-CLI associative arguments.
+   */
+  public function download($args, $assoc_args) {
+
+    // Grab associative arguments.
+    $install = (bool) \WP_CLI\Utils\get_flag_value($assoc_args, 'install', FALSE);
+
+    // Tease out URL when present.
+    $key_or_name = $args[0];
+    $url = '';
+    if (FALSE !== strpos($args[0], '@')) {
+      list ($key_or_name, $url) = explode('@', $args[0], 2);
+    }
+
+    // Build API vars.
+    $vars = 'key=' . $key_or_name;
+    if (!empty($url)) {
+      $vars .= ' url=' . $url;
+    }
+    if (empty($install)) {
+      $vars .= ' install=0';
+    }
+
+    // Use "wp civicrm api" to do the download.
+    $command = 'civicrm api extension.download ' . $vars . ' --format=json --quiet';
+    $options = ['launch' => FALSE, 'return' => TRUE];
+    $result = WP_CLI::runcommand($command, $options);
+
+    // Convert to array.
+    $result = json_decode($result, TRUE);
+    if (JSON_ERROR_NONE !== json_last_error()) {
+      WP_CLI::error(sprintf(WP_CLI::colorize('Failed to decode JSON: %y%s.%n'), json_last_error_msg()));
+    }
+
+    // Show error if present.
+    if (!empty($result['is_error']) && 1 === (int) $result['is_error']) {
+      WP_CLI::error(sprintf(WP_CLI::colorize('Failed to download CiviCRM Extension: %y%s%n'), $result['error_message']));
+    }
+
+    if (empty($install)) {
+      WP_CLI::success('CiviCRM Extension downloaded.');
+    }
+    else {
+      WP_CLI::success('CiviCRM Extension downloaded and installed.');
+    }
+
+  }
 
   /**
    * List the set of CiviCRM Extensions.
